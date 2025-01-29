9d44a13a5363f08cb342bc7138ca2cfd281629ea
Juliya_Medvedieva <medvedieva.juliya@gmail.com>
2025-01-29 18:08:18 +0100

    Resolved conflict  sayHello function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //comment number 2 (test editor)
 function sayHello(name, surname) {
     //Some_comment
-    console.log ('${name} S{surname}');
+    console.log (name, surname);
 }
 
 sayHello('Robot', 'Dreams');
